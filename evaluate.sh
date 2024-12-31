7e0a81f1fb891dcd406bd6661cf48a924d5ba6a1
HychTus <hychtus@qq.com>
2024-12-31 23:11:13 +0800

    Sync

--- evaluate.sh
+++ evaluate.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 image_path="/data/chy/others/MML-Assignment3/datasets/train2014"
-# weight_path="/data/chy/others/MML-Assignment3/results/weights"
 result_path="/data/chy/others/MML-Assignment3/results/visual_results/"
-num_samples=20
+num_samples=10
 
-# python mml/evaluate.py -S S -I $image_path \
-#                        -R "$result_path" \
-#                        -C "$result_path/origin_small/epoch_10.pth" \
-#                        -N $num_samples
+python mml/evaluate.py -S S -I $image_path \
+                       -R "$result_path" \
+                       -C "GPT2_SCLIP_best.pt" \
+                       -N $num_samples
 
 
 python mml/evaluate.py -S L -I $image_path \
                        -R "$result_path" \
-                       -C "epoch_9.pt" \
+                       -C "GPT2M_LCLIP_best.pt" \
                        -N $num_samples
 
-# python mml_Qwen/evaluate.py -S S -I $image_path \
-#                             -R "$result_path/Qwen_small" \
-#                             -C "$weight_path/Qwen_small/epoch_7.pth" \
-#                             -N $num_samples
+python mml_Qwen/evaluate.py -S S -I $image_path \
+                            -R "$result_path" \
+                            -C "Qwen_SCLIP_best" \
+                            -N $num_samples
 
 
-# python mml_Qwen/evaluate.py -S L -I $image_path \
-#                             -R "$result_path" \
-#                             -C "epoch_10.pt" \
-#                             -N $num_samples
+python mml_Qwen/evaluate.py -S L -I $image_path \
+                            -R "$result_path" \
+                            -C "Qwen_LCLIP_best.pt" \
+                            -N $num_samples
 
-# python mml_QFormer/evaluate.py -S S -I $image_path \
-#                                -R "$result_path/QFormer_small" \
-#                                -C "$weight_path/QFormer_small/epoch_10.pth" \
-#                                -N $num_samples
-# 
-# python mml_QFormer/evaluate.py -S L -I $image_path \
-#                                -R "$result_path/QFormer_large" \
-#                                -C "$weight_path/QFormer_large/epoch_10.pth" \
-#                                -N $num_samples
+python mml_QFormer/evaluate.py -S S -I $image_path \
+                               -R "$result_path" \
+                               -C "Qwen_SCLIP_QFormer_best.pt" \
+                               -N $num_samples
+
+python mml_QFormer/evaluate.py -S L -I $image_path \
+                               -R "$result_path" \
+                               -C "Qwen_LCLIP_QFormer_best.pt" \
+                               -N $num_samples
